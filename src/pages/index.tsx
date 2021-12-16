624696647efa14c176ca9451c44e31c6e416f473
Aaron-K-T-Berry <aaron.abm@gmail.com>
2021-12-16 21:22:36 +1100

    cleaned up content from init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,80 +1,12 @@
 import * as React from "react"
 import { PageProps } from "gatsby"
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const headingAccentStyles = {
-  color: "#663399",
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-// data
 const links = [
   {
     text: "Intro about me",
     url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description: "I'm Aaron, I'm a software engineering working in the data engineering and DevOps space building and automating software in the cloud.",
+    description:
+      "I'm Aaron, I'm a software engineering working in the data engineering and DevOps space building and automating software in the cloud.",
     color: "#E95800",
   },
   {
@@ -124,35 +56,16 @@ const links = [
   },
 ]
 
-// markup
 export default (props: PageProps) => {
   return (
     <main style={pageStyles}>
       <title>Home Page</title>
       <h1 style={headingStyles}>
-        Congratulations
+        Portfolio
         <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
+        <span style={headingAccentStyles}>— Aaron Berry </span>
       </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
       <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
         {links.map((link) => (
           <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
             <span>
@@ -169,4 +82,47 @@ export default (props: PageProps) => {
       </ul>
     </main>
   )
+}
+
+const pageStyles = {
+  color: "#232129",
+  padding: 96,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+}
+
+const headingStyles = {
+  marginTop: 0,
+  marginBottom: 64,
+  maxWidth: 320,
+}
+
+const headingAccentStyles = {
+  color: "#663399",
+}
+
+const listStyles = {
+  marginBottom: 96,
+  paddingLeft: 0,
+}
+
+const listItemStyles = {
+  fontWeight: 300,
+  fontSize: 24,
+  maxWidth: 560,
+  marginBottom: 30,
+}
+
+const linkStyle = {
+  color: "#8954A8",
+  fontWeight: "bold",
+  fontSize: 16,
+  verticalAlign: "5%",
+}
+
+const descriptionStyle = {
+  color: "#232129",
+  fontSize: 14,
+  marginTop: 10,
+  marginBottom: 0,
+  lineHeight: 1.25,
 }
