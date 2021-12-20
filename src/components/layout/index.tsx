9f8467bf75993810593fc1105c70edc0ce1b8a00
Aaron-K-T-Berry <aaron.abm@gmail.com>
2021-12-20 19:52:35 +1100

    cleaned up styling hirearchy

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,6 +1,6 @@
 import React from "react"
-
 import "./global.css"
+import "./shared.css"
 
 export const Layout: React.FunctionComponent = ({ children }) => {
   return <div>{children}</div>
--- src/components/sections/layout.tsx
+++ src/components/sections/layout.tsx
@@ -1,7 +1,5 @@
 import * as React from "react"
 
-import "./shared.css"
-
 export const Layout: React.FunctionComponent<{
   heading: string
 }> = (props) => {
