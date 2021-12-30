39c8413fd9b6af8265dea8bb144b083509855d84
Aaron-K-T-Berry <aaron.abm@gmail.com>
2021-12-30 17:33:34 +1100

    cleaned up styling to be responsive for about us

--- src/components/sections/about-me/index.tsx
+++ src/components/sections/about-me/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { StaticImage } from "gatsby-plugin-image"
 
-import * as AboutMeStyles from "./hero.module.css"
+import * as styles from "./hero.module.css"
 
 interface AboutMeProps {
   heading: string
@@ -11,28 +11,30 @@ interface AboutMeProps {
 
 export const AboutMe: React.FunctionComponent<AboutMeProps> = (props) => {
   return (
-    <div className={AboutMeStyles.hero}>
-      <StaticImage
-        className={AboutMeStyles.heroImg}
-        layout="fullWidth"
-        alt="Astronaut boy working on laptop"
-        src={"../../../images/astro.jpg"}
-        formats={["auto", "webp", "avif"]}
-      />
-
-      <div className={AboutMeStyles.bio}>
-        <div className="section_heading">
-          <a className={AboutMeStyles.headingLink} id={props.headingId}></a>
-          <h2>{props.heading}</h2>
+    <div className={styles.wrapper}>
+      <div className={styles.aboutMeWrapper}>
+        <div className={styles.bioWrapper}>
+          <a className={styles.headingLink} id={props.headingId}></a>
+          <div className="section_heading">
+            <h2>{props.heading}</h2>
+          </div>
+          <div>
+            <span>
+              {props.body.map((line) => (
+                <p>{line}</p>
+              ))}
+            </span>
+          </div>
         </div>
-
-        <div>
-          <span>
-            {props.body.map((line) => (
-              <p>{line}</p>
-            ))}
-          </span>
-        </div>
+      </div>
+      <div className={styles.imageWrapper}>
+        <StaticImage
+          className={styles.heroImg}
+          layout="fullWidth"
+          alt="Astronaut boy working on laptop"
+          src={"../../../images/astro.jpg"}
+          formats={["auto", "webp", "avif"]}
+        />
       </div>
     </div>
   )
