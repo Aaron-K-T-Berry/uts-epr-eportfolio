037da15668e3256fa36a1877094a2a1eff76bed7
Aaron-K-T-Berry <aaron.abm@gmail.com>
2022-01-05 17:23:52 +1100

    added link

--- src/components/common/footer/index.tsx
+++ src/components/common/footer/index.tsx
@@ -6,8 +6,14 @@ export const Footer: React.FunctionComponent<{}> = () => {
     <footer>
       <div className="footer-wrapper">
         <p>
-          &#169; 2021 - 2022 | Created by Aaron Berry, Checkout the repo{" "}
-          <a>here</a>
+          {"&#169; 2021 - 2022 | Created by Aaron Berry, Checkout the repo "}
+          <a
+            href="https://github.com/Aaron-K-T-Berry/epr-eportfolio"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            here
+          </a>
         </p>
       </div>
     </footer>
