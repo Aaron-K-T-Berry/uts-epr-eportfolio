ac6e7a19271e8529040db14deecc7b2405f543af
Aaron-K-T-Berry <aaron.abm@gmail.com>
2022-01-19 11:13:13 +1100

    cleaned up tech tooltips

---
+++ src/components/common/tech-icon-list/data.tsx
@@ -0,0 +1,171 @@
+import * as React from "react"
+import icons from "./icons"
+
+export default [
+  // Languages
+  {
+    name: "javascript",
+    displayName: "Javascript",
+    icon: <i className="devicon-javascript-plain" />,
+  },
+  {
+    name: "typescript",
+    displayName: "Typescript",
+    icon: <i className="devicon-typescript-plain" />,
+  },
+  {
+    name: "python",
+    displayName: "Python",
+    icon: <i className="devicon-python-plain" />,
+  },
+  {
+    name: "java",
+    displayName: "Java",
+    icon: <i className="devicon-java-plain" />,
+  },
+  {
+    name: "bash",
+    displayName: "Bash",
+    icon: <i className="devicon-bash-plain" />,
+  },
+  {
+    name: "markdown",
+    displayName: "Markdown",
+    icon: <i className="devicon-markdown-original" />,
+  },
+
+  // Frameworks
+  {
+    name: "react",
+    displayName: "React",
+    icon: <i className="devicon-react-original" />,
+  },
+  {
+    name: "express",
+    displayName: "ExpressJs",
+    icon: <i className="devicon-express-original" />,
+  },
+  {
+    name: "jest",
+    displayName: "Jest",
+    icon: <i className="devicon-jest-plain" />,
+  },
+  {
+    name: "gatsby",
+    displayName: "Gatsby",
+    icon: <i className="devicon-gatsby-plain" />,
+  },
+  {
+    name: "materialui",
+    displayName: "Material Ui",
+    icon: <i className="devicon-materialui-plain" />,
+  },
+  {
+    name: "dbt",
+    displayName: "Dbt",
+    icon: <icons.DbtSvg />,
+  },
+  {
+    name: "liquidbase",
+    displayName: "LiquidbaseSvg",
+    icon: <icons.LiquidbaseSvg />,
+  },
+
+  // Cloud providers
+  {
+    name: "aws",
+    displayName: "AWS",
+    icon: <i className="devicon-amazonwebservices-plain" />,
+  },
+  {
+    name: "azure",
+    displayName: "Azure",
+    icon: <i className="devicon-azure-plain" />,
+  },
+  {
+    name: "digitalocean",
+    displayName: "Digital Ocean",
+    icon: <i className="devicon-digitalocean-plain" />,
+  },
+  {
+    name: "firebase",
+    displayName: "Firebase",
+    icon: <i className="devicon-firebase-plain" />,
+  },
+  { name: "lambda", displayName: "Lambda", icon: <icons.AWSLambdaSvg /> },
+  {
+    name: "gcp",
+    displayName: "GCP",
+    icon: <i className="devicon-googlecloud-plain" />,
+  },
+
+  // Operating system and tools
+  {
+    name: "docker",
+    displayName: "Docker",
+    icon: <i className="devicon-docker-plain" />,
+  },
+  {
+    name: "linux",
+    displayName: "Linux",
+    icon: <i className="devicon-linux-plain" />,
+  },
+  {
+    name: "git",
+    displayName: "Git",
+    icon: <i className="devicon-git-plain" />,
+  },
+
+  // Other
+  {
+    name: "linkedin",
+    displayName: "LinkedIn",
+    icon: <i className="devicon-linkedin-plain" />,
+  },
+  {
+    name: "github",
+    displayName: "Github",
+    icon: <i className="devicon-github-plain" />,
+  },
+  {
+    name: "twitter",
+    displayName: "Twitter",
+    icon: <i className="devicon-twitter-plain" />,
+  },
+  {
+    name: "medium",
+    displayName: "",
+    icon: <icons.FontAwesomeIcon icon={icons.faMedium} />,
+  },
+  {
+    name: "computer",
+    displayName: "",
+    icon: <icons.FontAwesomeIcon icon={icons.faDesktop} />,
+  },
+  {
+    name: "word",
+    displayName: "",
+    icon: <icons.FontAwesomeIcon icon={icons.faFileWord} />,
+  },
+  {
+    name: "pdf",
+    displayName: "",
+    icon: <icons.FontAwesomeIcon icon={icons.faFilePdf} />,
+  },
+  { name: "packer", displayName: "", icon: <icons.PackerSvg /> },
+  { name: "terraform", displayName: "", icon: <icons.TerraformSvg /> },
+  { name: "proxmox", displayName: "", icon: <icons.ProxmoxSvg /> },
+  { name: "swagger", displayName: "", icon: <icons.SwaggerSvg /> },
+  { name: "airflow", displayName: "", icon: <icons.AirflowSvg /> },
+  { name: "datadog", displayName: "Datadog", icon: <icons.DatadogSvg /> },
+  {
+    name: "snowflake",
+    displayName: "Snowflake",
+    icon: <icons.SnowflakeSvg />,
+  },
+  {
+    name: "mysql",
+    displayName: "mysql",
+    icon: <i className="devicon-mysql-plain" />,
+  },
+]
--- src/components/common/tech-icon-list/index.tsx
+++ src/components/common/tech-icon-list/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
-import icons from "./icons"
 import { makeStyles } from "@mui/styles"
+import data from "./data"
 
 export const TechIconList: React.FunctionComponent<TechIconListProps> = (
   props
@@ -26,179 +26,10 @@ export const TechIconList: React.FunctionComponent<TechIconListProps> = (
     ...props.styles,
   } as StylesProps)
 
-  const techIcons = [
-    // Languages
-    {
-      name: "javascript",
-      displayName: "Javascript",
-      icon: <i className="devicon-javascript-plain" />,
-    },
-    {
-      name: "typescript",
-      displayName: "Typescript",
-      icon: <i className="devicon-typescript-plain" />,
-    },
-    {
-      name: "python",
-      displayName: "Python",
-      icon: <i className="devicon-python-plain" />,
-    },
-    {
-      name: "java",
-      displayName: "Java",
-      icon: <i className="devicon-java-plain" />,
-    },
-    {
-      name: "bash",
-      displayName: "Bash",
-      icon: <i className="devicon-bash-plain" />,
-    },
-    {
-      name: "markdown",
-      displayName: "Markdown",
-      icon: <i className="devicon-markdown-original" />,
-    },
-
-    // Frameworks
-    {
-      name: "react",
-      displayName: "React",
-      icon: <i className="devicon-react-original" />,
-    },
-    {
-      name: "express",
-      displayName: "ExpressJs",
-      icon: <i className="devicon-express-original" />,
-    },
-    {
-      name: "jest",
-      displayName: "Jest",
-      icon: <i className="devicon-jest-plain" />,
-    },
-    {
-      name: "gatsby",
-      displayName: "Gatsby",
-      icon: <i className="devicon-gatsby-plain" />,
-    },
-    {
-      name: "materialui",
-      displayName: "Material Ui",
-      icon: <i className="devicon-materialui-plain" />,
-    },
-    {
-      name: "dbt",
-      displayName: "Dbt",
-      icon: <icons.DbtSvg />,
-    },
-    {
-      name: "liquidbase",
-      displayName: "LiquidbaseSvg",
-      icon: <icons.LiquidbaseSvg />,
-    },
-
-    // Cloud providers
-    {
-      name: "aws",
-      displayName: "AWS",
-      icon: <i className="devicon-amazonwebservices-plain" />,
-    },
-    {
-      name: "azure",
-      displayName: "Azure",
-      icon: <i className="devicon-azure-plain" />,
-    },
-    {
-      name: "digitalocean",
-      displayName: "Digital Ocean",
-      icon: <i className="devicon-digitalocean-plain" />,
-    },
-    {
-      name: "firebase",
-      displayName: "Firebase",
-      icon: <i className="devicon-firebase-plain" />,
-    },
-    { name: "lambda", displayName: "Lambda", icon: <icons.AWSLambdaSvg /> },
-    {
-      name: "gcp",
-      displayName: "GCP",
-      icon: <i className="devicon-googlecloud-plain" />,
-    },
-
-    // Operating system and tools
-    {
-      name: "docker",
-      displayName: "Docker",
-      icon: <i className="devicon-docker-plain" />,
-    },
-    {
-      name: "linux",
-      displayName: "Linux",
-      icon: <i className="devicon-linux-plain" />,
-    },
-    {
-      name: "git",
-      displayName: "Git",
-      icon: <i className="devicon-git-plain" />,
-    },
-
-    // Other
-    {
-      name: "linkedin",
-      displayName: "LinkedIn",
-      icon: <i className="devicon-linkedin-plain" />,
-    },
-    {
-      name: "github",
-      displayName: "Github",
-      icon: <i className="devicon-github-plain" />,
-    },
-    {
-      name: "twitter",
-      displayName: "Twitter",
-      icon: <i className="devicon-twitter-plain" />,
-    },
-    {
-      name: "medium",
-      displayName: "",
-      icon: <icons.FontAwesomeIcon icon={icons.faMedium} />,
-    },
-    {
-      name: "computer",
-      displayName: "",
-      icon: <icons.FontAwesomeIcon icon={icons.faDesktop} />,
-    },
-    {
-      name: "word",
-      displayName: "",
-      icon: <icons.FontAwesomeIcon icon={icons.faFileWord} />,
-    },
-    {
-      name: "pdf",
-      displayName: "",
-      icon: <icons.FontAwesomeIcon icon={icons.faFilePdf} />,
-    },
-    { name: "packer", displayName: "", icon: <icons.PackerSvg /> },
-    { name: "terraform", displayName: "", icon: <icons.TerraformSvg /> },
-    { name: "proxmox", displayName: "", icon: <icons.ProxmoxSvg /> },
-    { name: "swagger", displayName: "", icon: <icons.SwaggerSvg /> },
-    { name: "airflow", displayName: "", icon: <icons.AirflowSvg /> },
-    { name: "datadog", displayName: "Datadog", icon: <icons.DatadogSvg /> },
-    {
-      name: "snowflake",
-      displayName: "Snowflake",
-      icon: <icons.SnowflakeSvg />,
-    },
-    {
-      name: "mysql",
-      displayName: "mysql",
-      icon: <i className="devicon-mysql-plain" />,
-    },
-  ]
-
   return (
     <div className={classes.list}>
       {props.technologies.map((tech) => {
-        const iconDetails = techIcons.filter((value) => value.name == tech)[0]
+        const iconDetails = data.filter((value) => value.name == tech)[0]
         if (iconDetails) {
           return (
             <div className={classes.icon}>
@@ -218,11 +49,11 @@ const useStyles = makeStyles({
     flexDirection: "row",
     flexWrap: "wrap",
     textAlign: "center",
-    padding: (props: StylesProps) => props.listPadding || "10px 0 0 0",
+    padding: (props: StylesProps) => props.listPadding || "10px 0 10px 0",
   },
   icon: (props: StylesProps) => ({
     width: props.spreadItems ? "30%" : null,
-    padding: props.iconPadding || "0 10px 10px 0",
+    padding: props.iconPadding || "0 10px 0 0",
     "& i, svg": {
       width: props.iconWidth || 40,
       height: props.iconWidth || 40,
