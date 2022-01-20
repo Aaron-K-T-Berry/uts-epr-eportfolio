d5c438a26a41be4cc22cde6104e74184b8f409e5
Aaron-K-T-Berry <aaron.abm@gmail.com>
2022-01-20 19:13:33 +1100

    mkaing link component more configureable

--- src/components/common/footer/index.tsx
+++ src/components/common/footer/index.tsx
@@ -7,8 +7,8 @@ export const Footer: React.FunctionComponent<{}> = () => {
     <footer>
       <div className="footer-wrapper">
         <p>
-          &#169; {"2021 - 2022 | Created by Aaron Berry, Checkout the repo "}
-          <Link href="https://github.com/Aaron-K-T-Berry/epr-eportfolio" text="here" outbound={true} chevron={true} />
+          &#169; {"2021 - 2022 | Created by Aaron Berry, "}
+          <Link href="https://github.com/Aaron-K-T-Berry/epr-eportfolio" text="Checkout the repo here" outbound={true} chevron={true} />
         </p>
       </div>
     </footer>
--- src/components/common/link/index.tsx
+++ src/components/common/link/index.tsx
@@ -6,7 +6,14 @@ import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons"
 export const Link: React.FunctionComponent<LinkProps> = (props) => {
   if (props.outbound) {
     return <OutboundLink href={props.href} target="blank" rel="noopener noreferrer">
-      {props.chevron ? (<>{props.text} <FontAwesomeIcon icon={faExternalLinkAlt} size="xs" /></>) : (<>{props.text}</>)}
+      {
+        props.children ?
+          (props.children) :
+          (props.chevron ?
+            (<>{props.text} <FontAwesomeIcon icon={faExternalLinkAlt} size="xs" /></>) :
+            (<>{props.text}</>)
+          )
+      }
     </OutboundLink>
   } else {
     return <a href={props.href}>
@@ -18,7 +25,7 @@ export const Link: React.FunctionComponent<LinkProps> = (props) => {
 
 interface LinkProps {
   href: string
-  text: string
+  text?: string
   outbound?: boolean
   chevron?: boolean
 }
